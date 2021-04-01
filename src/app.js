2aafcf39f09ddb30ce2d24de3807fd6ce61e8faa
jhonarendra <jhonarendra@gmail.com>
2021-04-01 17:58:31 +0800

    desain awal

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 import 'regenerator-runtime'
-global.jQuery = require('jquery')
+global.jQuery = require('jquery') // bootstrapnya minta jquery
 require('bootstrap/dist/css/bootstrap.css');
 import './styles/style.css'
 import main from './script/view/main.js'
