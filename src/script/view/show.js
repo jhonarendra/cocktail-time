d8a715a987c89e30547f03177c5a3223383756f0
jhonarendra <jhonarendra@gmail.com>
2021-04-03 12:08:05 +0800

    matching

--- src/script/view/show.js
+++ src/script/view/show.js
@@ -12,10 +12,15 @@ const show = {
 		id: ''
 	},
 	el: {
+		bgImage: '',
+		ctHeaderIcon: '',
 		btnBackHeader: '',
 		ctName: '',
 		ctIngredient: '',
 		ctAlcohol: '',
+		ingredients: '',
+		glass: '',
+		instruction: '',
 
 	},
 	method: {
@@ -28,10 +33,15 @@ const show = {
 			show.prop.id = param.id
 		},
 		trigger: () => {
+			show.el.bgImage = document.querySelector('.bg-image')
+			show.el.ctHeaderIcon = document.querySelector('.ct-header-icon')
 			show.el.btnBackHeader = document.querySelector('#btn-back-header')
 			show.el.ctName = document.querySelector('#ct-name')
 			show.el.ctIngredient = document.querySelector('#ct-ingredient')
 			show.el.ctAlcohol = document.querySelector('#ct-alcohol')
+			show.el.ingredients = document.querySelector('#ingredients')
+			show.el.glass = document.querySelector('#glass')
+			show.el.instruction = document.querySelector('#instruction')
 
 			show.el.btnBackHeader.addEventListener('click', () => {
 				changePage.method.setPage('search')
@@ -43,7 +53,28 @@ const show = {
 			let e = show.data.show
 			console.log(e)
 
+
+			show.el.bgImage.style.backgroundImage = `url('${e.strDrinkThumb}')`
+			show.el.ctHeaderIcon.setAttribute('src', e.strDrinkThumb)
+
 			show.el.ctName.innerHTML = e.strDrink
+			show.el.ctIngredient.innerHTML = ''
+			show.el.ingredients.innerHTML = ''
+			e.ingredients.forEach(e => {
+				show.el.ctIngredient.innerHTML += e.name + ', '
+				show.el.ingredients.innerHTML += `
+				<li class="p-3">
+					<img class="ing-icon" src="/src/img/sample/test2.jpg">
+					<span class="ing-name">${e.name}</span>
+					<span class="ing-measure">${(e.measure == null) ? '' : e.measure}</span>
+				</li>
+				`
+			})
+			show.el.ctAlcohol.innerHTML = e.strAlcoholic
+
+			show.el.glass.innerHTML = e.strGlass
+			show.el.instruction.innerHTML = e.strInstructions
+			
 
 		}
 	}
