cd326a2b3422a07479ec9eb934e6a2531cb6e24f
jhonarendra <jhonarendra@gmail.com>
2021-04-03 14:16:27 +0800

    bug fixed

--- src/script/data/api.js
+++ src/script/data/api.js
@@ -46,14 +46,79 @@ class Api {
                     .then(res => res.json())
                 promises.push(promise2)
             }
+            if(data.strIngredient3){
+                let promise3 = fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?i=${data.strIngredient3}`)
+                    .then(res => res.json())
+                promises.push(promise3)
+            }
+            if(data.strIngredient4){
+                let promise4 = fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?i=${data.strIngredient4}`)
+                    .then(res => res.json())
+                promises.push(promise4)
+            }
+            if(data.strIngredient5){
+                let promise5 = fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?i=${data.strIngredient5}`)
+                    .then(res => res.json())
+                promises.push(promise5)
+            }
+            if(data.strIngredient6){
+                let promise6 = fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?i=${data.strIngredient6}`)
+                    .then(res => res.json())
+                promises.push(promise6)
+            }
+            if(data.strIngredient7){
+                let promise7 = fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?i=${data.strIngredient7}`)
+                    .then(res => res.json())
+                promises.push(promise7)
+            }
+            if(data.strIngredient8){
+                let promise8 = fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?i=${data.strIngredient8}`)
+                    .then(res => res.json())
+                promises.push(promise8)
+            }
+            if(data.strIngredient9){
+                let promise9 = fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?i=${data.strIngredient9}`)
+                    .then(res => res.json())
+                promises.push(promise9)
+            }
+            if(data.strIngredient10){
+                let promise10 = fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?i=${data.strIngredient10}`)
+                    .then(res => res.json())
+                promises.push(promise10)
+            }
+            if(data.strIngredient11){
+                let promise11 = fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?i=${data.strIngredient11}`)
+                    .then(res => res.json())
+                promises.push(promise11)
+            }
+            if(data.strIngredient12){
+                let promise12 = fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?i=${data.strIngredient12}`)
+                    .then(res => res.json())
+                promises.push(promise12)
+            }
+            if(data.strIngredient13){
+                let promise13 = fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?i=${data.strIngredient13}`)
+                    .then(res => res.json())
+                promises.push(promise13)
+            }
+            if(data.strIngredient14){
+                let promise14 = fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?i=${data.strIngredient14}`)
+                    .then(res => res.json())
+                promises.push(promise14)
+            }
+            if(data.strIngredient15){
+                let promise15 = fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?i=${data.strIngredient15}`)
+                    .then(res => res.json())
+                promises.push(promise15)
+            }
 
             return Promise.all(promises).then(res => {
                 let ingredients = []
 
-                res.forEach(e => {
+                res.forEach((e, i) => {
                     let ing = e.ingredients[0]
-                    ing.measure = data.strMeasure1
-                    ing.strThumb = 'https://www.thecocktaildb.com/images/ingredients/'+data.strIngredient1+'-Small.png'
+                    ing.measure = data['strMeasure'+parseInt(i+1)]
+                    ing.strThumb = 'https://www.thecocktaildb.com/images/ingredients/'+data['strIngredient'+parseInt(i+1)]+'-Small.png'
                     ingredients.push(ing)
                 })
 
